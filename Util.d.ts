e6d280c092de56c7d93126cc6cd91dc21a28b11b
MBR <buildmachineubuntu@gmail.com>
2020-06-29 16:04:12 +0200

    change typings completely

--- Util.d.ts
+++
@@ -1,4 +0,0 @@
-export function IPFromRequest(req: any): string;
-export function CleanIP(ip: string): string;
-export function IsIPAddress(ip: string): boolean;
-export function IsObject(o: any): boolean;
